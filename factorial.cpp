ce68fcb4f743ef0ed3104e4501fd25ff3e897a5c
Anusha Talagapu <anushatalagapu@gmail.com>
2024-05-06 19:46:57 +0530

    factorial using recursion

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n==0){
+        return  1;
+
+    }
+    int sp=factorial(n-1);
+    int bp=n*sp;
+    return bp;
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans=factorial(n);
+    cout<<ans;
+}
